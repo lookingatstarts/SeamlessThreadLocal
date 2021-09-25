ffac7392fd96f7bda6177f2f00cbbf3eca8200af
zhangbingbing <lookingatstarts@163.com>
2021-09-25 17:55:19 +0800

    实现基本功能

--- src/main/java/com/easy/ParentValueCopy.java
+++
@@ -1,6 +0,0 @@
-package com.easy;
-
-public interface ParentValueCopy<T> {
-
-  T copy(T parentValue);
-}
--- src/main/java/com/easy/SeamlessThreadLocal.java
+++ src/main/java/com/easy/SeamlessThreadLocal.java
@@ -1,52 +1,23 @@
 package com.easy;
 
-import com.easy.vo.Snapshot;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.WeakHashMap;
+@SuppressWarnings("unchecked")
+public class SeamlessThreadLocal<T> extends InheritableThreadLocal<T> implements Enhanced {
 
-public interface SeamlessThreadLocal<T> extends Enhanced {
-
-  /**
-   * 使用InheritableThreadLocal是为了父线程可以传递执行上下文子线程
-   */
-  InheritableThreadLocal<WeakHashMap<SeamlessThreadLocal<?>, Object>> holder = new InheritableThreadLocal<WeakHashMap<SeamlessThreadLocal<?>, Object>>() {
-
-    /***
-     * 父线程传递值给子线程，浅拷贝，会破坏线程封闭性
-     */
-    @Override
-    protected WeakHashMap<SeamlessThreadLocal<?>, Object> childValue(
-        WeakHashMap<SeamlessThreadLocal<?>, Object> parentValue) {
-      return new WeakHashMap<>(parentValue);
-    }
-
-    @Override
-    protected WeakHashMap<SeamlessThreadLocal<?>, Object> initialValue() {
-      return new WeakHashMap<>();
-    }
-  };
-
-  /**
-   * 快照当前线程拥有的SeamlessThreadLocal
-   */
-  default Snapshot snapshot() {
-    WeakHashMap<SeamlessThreadLocal<?>, Object> seamlessThreadLocalMap = holder.get();
-    Map<SeamlessThreadLocal<?>, Object> snapshotMap = Collections.emptyMap();
-    if (seamlessThreadLocalMap != null && seamlessThreadLocalMap.size() != 0) {
-      snapshotMap = new HashMap<>(seamlessThreadLocalMap.size());
-      for (Entry<SeamlessThreadLocal<?>, Object> entry : seamlessThreadLocalMap.entrySet()) {
-        snapshotMap.put(entry.getKey(), entry.getValue());
-      }
-    }
-    return new Snapshot(snapshotMap);
+  @Override
+  public final void set(T t) {
+    Holder.addToHolder((SeamlessThreadLocal<Object>) this);
+    super.set(t);
   }
 
-  void set(T t);
+  @Override
+  public final T get() {
+    Holder.addToHolder((SeamlessThreadLocal<Object>) this);
+    return super.get();
+  }
 
-  T get();
-
-  void remove();
+  @Override
+  public final void remove() {
+    Holder.removeFromHolder((SeamlessThreadLocal<Object>) this);
+    super.remove();
+  }
 }
